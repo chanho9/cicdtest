f5558a71395432deea1053857a9b6a4e8851affb
chanho <chanho0@krri.re.kr>
2024-01-13 22:35:46 +0900

    test

--- tests/test_my_code.py
+++ tests/test_my_code.py
@@ -9,7 +9,7 @@ from my_module.function import add_numbers
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
-        self.assertEqual(add_numbers(1, 2), 3)
+        self.assertEqual(add_numbers(1, 2), 4)
 
     def test_add_negative_numbers(self):
         self.assertEqual(add_numbers(-1, -2), -3)
